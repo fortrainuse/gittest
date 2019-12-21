903b002d281faf08c20abeccaa99617758dd68b8
tina <fortrainuse@gmail.com>
2019-12-21 15:17:45 +0800

    2 ver branchTest

--- nine.java
+++ nine.java
@@ -14,7 +14,7 @@ public class NineNineLoop {
 		for (i = 1; i <= 9; i++) {
 			for (j = 1; j <= 9; j++)
 				System.out.print(i + "*" + j + "=" + i * j + "\t");
-			System.out.println("================");
+			System.out.println("================master==========");
 		}
 	}
 
